0449094e6cf0ff5536332298718d908a5d020848
Hardik Kanajariya <hkdevs@hardikkanajariya.in>
2026-05-04 21:53:30 +0530

    Update contact-form-config.ts

--- lib/contact-form-config.ts
+++ lib/contact-form-config.ts
@@ -1,4 +1,4 @@
 export const contactFormConfig = {
   web3FormsAccessKey: "2c26b364-f076-4523-861e-33c33a2b5b7b",
-  recaptchaV3SiteKey: "6LeSk9gsAAAAAMI-UwPCCmtww9YLVhJ9Jjmimlvx",
+  recaptchaV3SiteKey: "6LfU1tgsAAAAANJ5Er4s2cYYlKnoh8hrPTqnG5VJ",
 } as const;
